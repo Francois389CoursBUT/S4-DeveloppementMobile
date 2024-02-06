8e5517d1378646c12b3fa7d07addb2dfff4639e5
François SP <francois-sp@gmx.fr>
2024-02-06 16:49:34 +0100

    TP3 Cas nominal fini
    Il manque la gestion des erreurs

--- GestionInteractivite/app/src/main/java/com/fsp/gestioninteractivite/MainActivity.java
+++ GestionInteractivite/app/src/main/java/com/fsp/gestioninteractivite/MainActivity.java
@@ -4,30 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.fsp.gestioninteractivite.OutilAleatoire;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtResultat;
+    private EditText inputPrixInitial;
+
+    private TextView inputTauxReduction;
+
+    private TextView inputEconomieRealisee;
+
+    private TextView inputPrixAPayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtResultat = findViewById(R.id.txtResultat);
+
+        inputPrixInitial = findViewById(R.id.prix_init);
+        inputTauxReduction = findViewById(R.id.taux_reduc);
+        inputEconomieRealisee = findViewById(R.id.economie);
+        inputPrixAPayer = findViewById(R.id.a_payer);
     }
 
-    public void onClickPileFace(View bouton) {
-        txtResultat.setText("Le tirage à donné " + OutilAleatoire.tiragePileFace());
+    public void calculer(View view) {
+        double prixInitial = Double.parseDouble(inputPrixInitial.getText().toString());
+        double tauxReduction = Double.parseDouble(inputTauxReduction.getText().toString());
+
+        if (100 <= tauxReduction && 0 < prixInitial && 0 < tauxReduction) {
+            double economieRealisee = prixInitial * tauxReduction / 100;
+            double prixAPayer = prixInitial - economieRealisee;
+
+            inputEconomieRealisee.setText(String.valueOf(economieRealisee));
+            inputPrixAPayer.setText(String.valueOf(prixAPayer));
+        } else {
+            //Afficher un toast pour afficher un message d'erreur
+
+            Toast.makeText(this, "Le taux de réduction doit être supérieur ou égal à 100", Toast.LENGTH_LONG).show();
+
+        }
     }
 
-    public void onClickDe6(View bouton) {
-        txtResultat.setText("Le tirage à donné : " + OutilAleatoire.tirageAleatoire(6));
+    public void annuler(View view) {
+        inputPrixInitial.setText("");
+        inputTauxReduction.setText("");
+        inputEconomieRealisee.setText("");
+        inputPrixAPayer.setText("");
     }
 
-    public void onClickTirageLoto(View bouton) {
-        txtResultat.setText("Valeur conseillée pour le loto : " + OutilAleatoire.tirageAleatoire(49));
-    }
+
 }
