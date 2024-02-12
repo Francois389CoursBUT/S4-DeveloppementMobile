0f2e0781e5169d60b04c09a892f53182810efed3
François SP <francois-sp@gmx.fr>
2024-02-12 16:00:26 +0100

    Fin Exercice 2

--- TP4/ListeRessourceBUT/app/src/main/java/com/fsp/listeressourcebut/MainActivity.java
+++ TP4/ListeRessourceBUT/app/src/main/java/com/fsp/listeressourcebut/MainActivity.java
@@ -1,16 +1,20 @@
 package com.fsp.listeressourcebut;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView listeModule;
 
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private int[] coefTroncCommun;
     private int[] coefAxeA;
     private int[] coefAxeB;
+
+    ArrayAdapter<String> adaptateur;
 
 
     @Override
@@ -35,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         coefTroncCommun =  getResources().getIntArray(R.array.coef_tronc_commun);
         coefAxeA =  getResources().getIntArray(R.array.coef_parcours_A);
         coefAxeB =  getResources().getIntArray(R.array.coef_parcours_B);
+
+        adaptateur = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
     }
 
     public void clicTous(View view) {
@@ -43,22 +51,62 @@ public class MainActivity extends AppCompatActivity {
         liste.addAll(Arrays.asList(modulesAxeA));
         liste.addAll(Arrays.asList(modulesAxeB));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, liste);
-        listeModule.setAdapter(adapter);
+
+        adaptateur.clear();
+        adaptateur.addAll(liste);
+
+        listeModule.setAdapter(adaptateur);
+        listeModule.setOnItemClickListener(this);
     }
 
     public void clicTroncCommun(View view) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, modulesTroncCommun);
-        listeModule.setAdapter(adapter);
+        adaptateur.clear();
+        adaptateur.addAll(Arrays.asList(modulesTroncCommun));
+
+        listeModule.setAdapter(adaptateur);
+        listeModule.setOnItemClickListener(this);
     }
 
     public void clicAxeA(View view) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, modulesAxeA);
-        listeModule.setAdapter(adapter);
+        adaptateur.clear();
+        adaptateur.addAll(Arrays.asList(modulesAxeA));
+
+        listeModule.setAdapter(adaptateur);
+        listeModule.setOnItemClickListener(this);
     }
 
     public void clicAxeB(View view) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, modulesAxeB);
-        listeModule.setAdapter(adapter);
+        adaptateur.clear();
+        adaptateur.addAll(Arrays.asList(modulesAxeB));
+
+        listeModule.setAdapter(adaptateur);
+        listeModule.setOnItemClickListener(this);
+    }
+
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String module = (String) parent.getItemAtPosition(position);
+        int indexModule = Arrays.asList(modulesTroncCommun).indexOf(module);
+        int coefficient = 0;
+        String message = getString(R.string.message_toast_coef);
+
+        if (Arrays.asList(modulesTroncCommun).contains(module)) {
+            coefficient = coefTroncCommun[indexModule];
+        } else if (Arrays.asList(modulesAxeA).contains(module)) {
+            coefficient = coefAxeA[indexModule];
+        } else if (Arrays.asList(modulesAxeB).contains(module)) {
+            coefficient = coefAxeB[indexModule];
+        }
+
+        // On rétablie le coefficient à sa valeur correcte
+        double coefficientReel = coefficient / 10.0;
+
+        AlertDialog.Builder boiteDeDialogue = new AlertDialog.Builder(this);
+        boiteDeDialogue.setTitle("Ressource sélectionnée");
+        boiteDeDialogue.setMessage(String.format(message, module, coefficientReel));
+        boiteDeDialogue.setPositiveButton("OK", null);
+        boiteDeDialogue.show();
     }
 }
